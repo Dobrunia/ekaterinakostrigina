0b47c09b10ee6c5aaaea5594824db2cef6bc4638
dobrunia <dobkos@hotmail.com>
2020-09-25 12:57:16 +0300

    favicon

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <title>Ekaterina Kostrigina</title>
     <link href="css/style.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="lightbox/css/lightbox.css">
+	<link rel="icon" href="./img/favicon.ico" type="image/x-icon">
     <!-- Yandex.Metrika counter --> <script> (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(67249705, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/67249705" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
     <meta name="google-site-verification" content="gvBPXt5pc7cRFsyaOut8TA0UG6sMWvZNWZRzmBllroM" />
 </head>
--- navbar.php
+++ navbar.php
@@ -4,22 +4,22 @@
     <div>
         <button id="Toggle2" class="btn">ювелирные изделия</button>
         <nav id="products" class="disp">
-            <button class="btn" id="button0">все</button>
-            <button class="btn" id="button1">серьги</button>
-            <button class="btn" id="button2">подвески</button>
-            <button class="btn" id="button3">браслеты</button>
-            <button class="btn" id="button4">кольца</button>
-            <button class="btn" id="button5">колье</button>
-            <button class="btn" id="button6">бусы</button>
-            <button class="btn" id="button7">броши</button>
+            <button class="btn" id="button0"><i>все</i></button>
+            <button class="btn" id="button1"><i>серьги</i></button>
+            <button class="btn" id="button2"><i>подвески</i></button>
+            <button class="btn" id="button3"><i>браслеты</i></button>
+            <button class="btn" id="button4"><i>кольца</i></button>
+            <button class="btn" id="button5"><i>колье</i></button>
+            <button class="btn" id="button6"><i>бусы</i></button>
+            <button class="btn" id="button7"><i>броши</i></button>
         </nav>
     </div>
     <a href="about.php" class="btn">о нас</a>
     <div>
         <button id="Toggle3" class="btn">как нас найти</button>
         <nav id="find" class="disp">
-            <a href="findUsMoscow.php" class="btn">Москва</a>
-            <a href="findUsSaint-P.php" class="btn">Санкт-Петербург</a>
+            <a href="findUsMoscow.php" class="btn"><i>Москва</i></a>
+            <a href="findUsSaint-P.php" class="btn"><i>Санкт-Петербург</i></a>
         </nav>
     </div>
 </header>
